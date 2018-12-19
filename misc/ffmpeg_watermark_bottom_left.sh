35a76ba6c7e68d15f247c89494ad766b20f3409d
Ole Algoritme <olealgoritme@gmail.com>
2018-12-20 00:48:00 +0100

    updated video4

--- misc/ffmpeg_watermark_bottom_left.sh
+++ misc/ffmpeg_watermark_bottom_left.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #legger til watermark i (width-35px) x (height-30px)
 #watermark er ca 200px
-ffmpeg -i $1 -i ../Pictures/logo.png -filter_complex overlay=x=(main_w-35-overlay_w):y=(main_h-30-overlay_h) $1_watermark.mp4
+ffmpeg -i $1 -i logo.png -filter_complex "overlay=x=(main_w-35-overlay_w):y=(main_h-30-overlay_h)" $1_wm.mp4
